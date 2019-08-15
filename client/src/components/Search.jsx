1d9a24267fd71225390455595d7b0a42e66d1f80
Kate <krharney@gmail.com>
2019-08-15 11:18:26 -0400

    Make request for genres and update page

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class Search extends React.Component {
   constructor(props) {
@@ -6,26 +7,38 @@ class Search extends React.Component {
     this.state = {
       genres: []
     };
+    this.getGenres = this.getGenres.bind(this);
   }
+
   getGenres() {
-    //make an axios request in this component to get the list of genres from your endpoint GET GENRES
+    axios.get('/genres').then((response) => {
+      this.setState({
+        genres: response.data
+      })
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+
+  componentDidMount() {
+    this.getGenres();
   }
 
   render() {
     return (
       <div className="search">
-        <button onClick={() => {this.props.swapFavorites()}}>{this.props.showFaves ? "Show Results" : "Show Favorites"}</button>
-        <br/><br/>
+        <button onClick={() => { this.props.swapFavorites() }}>{this.props.showFaves ? "Show Results" : "Show Favorites"}</button>
+        <br /><br />
 
         {/* Make the select options dynamic from genres !!! */}
         {/* How can you tell which option has been selected from here? */}
 
         <select>
-          <option value="theway">The Way</option>
-          <option value="thisway">This Way</option>
-          <option value="thatway">That Way</option>
+          {this.state.genres.map((element) => {
+            return <option value={element.name} key={element.id}>{element.name}</option>
+          })}
         </select>
-        <br/><br/>
+        <br /><br />
 
         <button>Search</button>
 
--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request')
+var axios = require('axios');
 var app = express();
+var config = require('../config');
 
 // Sign up and get your moviedb API key here:
 // https://www.themoviedb.org/account/signup
@@ -19,28 +21,34 @@ app.use(express.static(__dirname + '/../client/dist'));
 
 //OPTION 1: Use regular routes
 
-app.get('/genres', function(req, res) {
+app.get('/genres', function (req, res) {
   // make an axios request to get the official list of genres from themoviedb
-  
   // use this endpoint. you will need your API key from signup: https://api.themoviedb.org/3/genre/movie/list
-
-  // send back
+  axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${config.API_KEY}&language=en-US`)
+    .then(function (response) {
+      console.log(config);
+      res.status(200).send(JSON.stringify(response.data.genres));
+    })
+    .catch(function (error) {
+      console.log(error);
+      res.sendStatus(500).end("error in the get genres");
+    });
 });
 
-app.get('/search', function(req, res) {
+app.get('/search', function (req, res) {
   // use this endpoint to search for movies by genres (using API key): https://api.themoviedb.org/3/discover/movie
 
   // and sort them by votes (worst first) using the search parameters in themoviedb API
 });
 
 
-app.post('/save', function(req, res) {
+app.post('/save', function (req, res) {
 
   //save movie as favorite
 
 });
 
-app.post('/delete', function(req, res) {
+app.post('/delete', function (req, res) {
 
   //remove movie from favorites
 
@@ -53,12 +61,12 @@ app.post('/delete', function(req, res) {
 
 //Routes
 
-const movieRoutes = require('./routes/movieRoutes.js');
+// const movieRoutes = require('./routes/movieRoutes.js');
 
-//Use routes
-app.use('/movies', movieRoutes);
+// //Use routes
+// app.use('/movies', movieRoutes);
 
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log('listening on port 3000!');
 });
